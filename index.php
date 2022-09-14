82ded9843d5b48a394f6c8340074784a01ac3fcc
Julian D Rojas <juliandra140201@gmail.com>
2022-09-14 17:43:59 -0500

    Navbar Success

--- index.php
+++ index.php
@@ -15,24 +15,24 @@
     <body>
         <!-- Header with Navbar -->
         <header class="header">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">Wukong Tattoo Studio</a>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+                <a class="navbar-brand" href="index.php">Wukong Tattoo Studio</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse d-flex justify-content-around" id="navbarNav">
+                <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="#home">HOME <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#artist">NUESTRO ARTISTA</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link" href="#gallery">PROYECTOS</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
+                            <a class="nav-link" href="#cotizaciones">COTIZAR PROCEDIMIENTO</a>
                         </li>
                     </ul>
                 </div>
